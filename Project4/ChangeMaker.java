bf7dfbf5045c2ab74e09a0cbcf2b62ebf4a94909
iguzmanl <iguzmanl@calpoly.edu>
2018-02-21 09:56:21 -0800

    :^) greedy solution and added tester woooooOO

--- Project4/ChangeMaker.java
+++ Project4/ChangeMaker.java
@@ -23,16 +23,50 @@ public class ChangeMaker {
 
       System.out.println("Enter a positive amount to be changed (enter 0 to quit):");
       int n = input.nextInt();
-      while(n != 0)
-         change_DP(n,coins);
+      while(n != 0){
+         //change_DP(n,coins);
+         printArr(coins,change_greedy(n,coins));
+         System.out.println("Enter a positive amount to be changed (enter 0 to quit):");
+         n = input.nextInt();
+
+      }
+
 
       System.out.println("Thanks for playing. Good Bye.");
 
 
    }
    public static int[] change_DP(int n, int[] d){
+      return d;
 
 
+   }
+   public static int[] change_greedy(int n, int[] d){
+      int arr[] = new int[d.length];
+      int i = 0;
+      while(n > 0 && i < arr.length){
+         if(d[i] <= n){
+            arr[i]++;
+            n -= d[i];
+         }
+         else
+            i++;
+      }
+      return arr;
+   }
+   public static void printArr(int[] d, int[] freq){
+      System.out.println("Optimal distribution: ");
+      int count = 0;
+      for(int i = 0; i < d.length-1; i++){
+         System.out.print(freq[i] + "*" + d[i] + "c+ ");
+         if(freq[i] > 0)
+            count += freq[i];
+
+      }
+      System.out.print(freq[d.length-1] + "*" + d[d.length-1] + "c\n");
+      if(freq[d.length-1] > 0)
+         count += freq[d.length-1];
+      System.out.println("Optimal coin count: " + count);
    }
 }
 
---
+++ Project4/Tester.java
@@ -0,0 +1,27 @@
+public class Tester{
+   public static void main(String args[]){
+      int[] US = {100, 50, 25, 10, 5, 1 };
+      int[] soviet = {100, 50, 20, 15, 10, 5, 3, 2, 1};
+      int[] pow = {64, 32, 16, 8, 4, 2, 1};
+      int[] noNickel = {100, 50, 25, 10, 1};
+      int[] someSet = {66, 35, 27, 18, 10, 1};
+      int USMatches, sovietMatches, powMatches, noNickelMatches, someSetMatches = 0;
+      for(int i = 1; i <= 200; i++){
+         USMatches += cointCounter(ChangeMaker.change_DP(i,US), ChangeMaker.change_greedy(i,US)) ? 1 : 0;
+         sovietMatches += cointCounter(ChangeMaker.change_DP(i,soviet), ChangeMaker.change_greedy(i,soviet)) ? 1 : 0;
+         powMatches += cointCounter(ChangeMaker.change_DP(i,pow), ChangeMaker.change_greedy(i,pow)) ? 1 : 0;
+         noNickelMatches += cointCounter(ChangeMaker.change_DP(i,noNickel), ChangeMaker.change_greedy(i,noNickel)) ? 1 : 0;
+         someSetMatches += cointCounter(ChangeMaker.change_DP(i,someSet), ChangeMaker.change_greedy(i,someSet)) ? 1 : 0;
+      }
+
+   }
+   public static boolean cointCounter(int[] freqA, int[] freqB){
+      int countA, countB = 0;
+      for(int i = 0; i < freq.length; i++){
+         countA += freqA[i];
+         countB += freqB[i];
+      }
+      return countA == countB;
+
+   }
+}
