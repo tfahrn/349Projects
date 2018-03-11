6fb8e40865bb392aa4f209f7f7e555b04e9cfcd5
Thomas Fahrner <33201730+tfahrn@users.noreply.github.com>
2018-03-11 14:57:11 -0700

    finished project; needs testing and header comments

--- Project5/DiGraph.java
+++ Project5/DiGraph.java
@@ -128,4 +128,158 @@ public class DiGraph {
 
       return topSort;
    }
+
+   private class VertexInfo {
+      //distance from s
+      private int distance;
+      //index of predecessor on path from s
+      private int parent;
+
+      public VertexInfo(int d, int p) {
+         this.distance = d;
+         this.parent = p;
+      }
+
+      public void setDistance(int d) {
+         this.distance = d;
+      }
+      public int getDistance() {
+         return this.distance;
+      }
+
+      public void setParent(int p) {
+         this.parent = p;
+      }
+      public int getParent() {
+         return this.parent;
+      }
+   }
+
+   private VertexInfo[] BFS(int s) {
+      int N = graph.length;
+      VertexInfo[] VA = new VertexInfo[N];
+
+      for(int i = 0; i < N; i++) {
+         VA[i] = new VertexInfo(-1, -1);
+      }
+
+      VA[s].setDistance(0);
+
+      LinkedList<Integer> queue = new LinkedList<Integer>();
+      queue.add(s);
+
+      while(queue.size() != 0) {
+         int u = queue.remove();
+
+         if(graph[u] != null) {
+            for(int v : graph[u]) {
+               if(VA[v].getDistance() == -1) {
+                  VA[v].setDistance(VA[u].getDistance() + 1);
+                  VA[v].setParent(u);
+                  queue.add(v);
+               }
+            }
+         }
+      }
+
+      return VA;
+   }
+
+   public boolean isTherePath(int from, int to) {
+      VertexInfo[] VA = BFS(from);
+
+      if(VA[to].getDistance() != -1) {
+         return true;
+      }
+
+      return false;
+   }
+
+   public int lengthOfPath(int from, int to) {
+      VertexInfo[] VA = BFS(from);
+      
+      return VA[to].getDistance();
+   }
+
+   public String printPath(int from, int to) {
+      VertexInfo[] VA = BFS(from);
+
+      if(VA[to].getDistance() == -1) {
+         return null;
+      }
+      else {
+         StringBuilder sb = new StringBuilder();
+
+         while(from != to) {
+            sb.append((to+1) + ">--");
+            to = VA[to].getParent();
+         }
+
+         sb.append((from+1));
+
+         return sb.reverse().toString();
+      }
+   }
+
+   private class TreeNode {
+      private int vertexNumber;
+      private LinkedList<TreeNode> children;
+
+      public TreeNode(int vertexNumber) {
+         this.vertexNumber = vertexNumber;
+      }
+
+      public void addChild(TreeNode child) {
+         if(this.children == null) {
+            this.children = new LinkedList<TreeNode>();
+         }
+
+         this.children.add(child);
+      } 
+
+      public LinkedList<TreeNode> getChildren() {
+         return this.children;
+      }
+   }
+
+   private TreeNode buildTree(int s) {
+      VertexInfo[] VA = BFS(s);
+      TreeNode[] allNodes = new TreeNode[VA.length];
+
+      for(int i = 0; i < VA.length; i++) {
+         allNodes[i] = new TreeNode(i);
+      }
+
+      for(int i = 0; i < VA.length; i++) {
+         for(int j = 0; j < VA.length; j++) {
+            if(i != j) {
+               if(VA[j].getParent() == i) {
+                  allNodes[i].addChild(allNodes[j]);
+               }
+            }
+         }
+      }
+
+      return allNodes[s];
+   }
+
+   public void printTree(int s) {
+      TreeNode root = buildTree(s);
+
+      printTreeRecur(root, 0);
+   }
+
+   private void printTreeRecur(TreeNode curRoot, int numSpaces) {
+      for(int i = 0; i < numSpaces; i++) {
+         System.out.print(" ");
+      }
+
+      System.out.println(curRoot.vertexNumber + 1);
+
+      if(curRoot.getChildren() != null) {
+         for(TreeNode child : curRoot.getChildren()) {
+            printTreeRecur(child, numSpaces + 4);
+         }
+      }
+   }
 }
--- Project5/DiGraphTest.java
+++ Project5/DiGraphTest.java
@@ -14,7 +14,7 @@ public class DiGraphTest {
 
       char nextInput = 'o';
       
-      System.out.println("Choose one of the following operations:\n- add edge (enter a)\n- delete edge (enter d)\n- edge count (enter e)\n- vertex count(enter v)\n- print graph (enter p)\n- topological sort (enter t)\n- Quit (enter q)");
+      System.out.println("Choose one of the following operations:\n- add edge (enter a)\n- delete edge (enter d)\n- edge count (enter e)\n- vertex count(enter v)\n- print graph (enter p)\n- topological sort (enter t)\n- is path (enter i)\n- length of path (enter l)\n- shortest path (enter s)\n- print breadth-first-tree (enter b)\n- Quit (enter q)");
 
       while(nextInput != 'q') {
 
@@ -67,6 +67,42 @@ public class DiGraphTest {
                System.out.print(topSort[i] + ", ");
             }
             System.out.println(topSort[topSort.length-1]);
+         }
+         else if(nextInput == 'i' || nextInput == 'l' || nextInput == 's') {
+            System.out.println("Enter the from index: ");
+            int from = input.nextInt()-1;
+            System.out.println("Enter the to index: ");
+            int to = input.nextInt()-1;
+            input.nextLine();
+
+            if(nextInput == 'i') {
+               if(myGraph.isTherePath(from, to)) {
+                  System.out.println("There is a path from " + (from + 1) + " to " + (to + 1));
+               }
+               else {
+                  System.out.println("No path exists from " + (from + 1) + " to " + (to + 1));
+               }
+            }
+            else if(nextInput == 'l') {
+               System.out.println("Length of path from " + (from+1) + " to " + (to + 1) + " is " + myGraph.lengthOfPath(from, to));
+            }
+            else if(nextInput == 's') {
+               String path = myGraph.printPath(from, to);
+
+               if(path == null) {
+                  System.out.println("No path exists from " + (from + 1) + " to " + (to + 1));
+               }
+               else {
+                  System.out.println("Path: " + path);
+               }
+            }
+         }
+         else if(nextInput == 'b') {
+            System.out.println("Enter the source index: ");
+            int from = input.nextInt()-1;
+            input.nextLine();
+
+            myGraph.printTree(from);
          }
          else if(nextInput != 'q'){
             System.out.println("Invalid menu choice");
